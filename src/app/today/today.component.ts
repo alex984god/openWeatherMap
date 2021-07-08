638045b2aa6d4653a2eac4e31cf8414b95ebf0d2
Miroslav Aleksovski <miroslav.aleksovski@corvus-info.com>
2021-07-08 10:34:38 +0200

    .

--- src/app/today/today.component.ts
+++ src/app/today/today.component.ts
@@ -1,32 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../weather.service';
+
 @Component({
   selector: 'app-today',
   templateUrl: './today.component.html',
   styleUrls: ['./today.component.scss']
 })
 export class TodayComponent implements OnInit {
-   lat;
-   lon;
-   weather;
-  constructor(private weatherService:WeatherService) { }
 
-  ngOnInit(): void {
-    this.geolocation();
-  }
-geolocation(){
-  if('geolocation' in navigator){
-    navigator.geolocation.watchPosition((success)=>{
-      this.lat=success.coords.latitude;
-      this.lon=success.coords.longitude;
-      this.weatherService.getWeatherDataByChords(this.lat, this.lon).subscribe(data=>{
-      this.weather=data;
-      console.log(this.weather);
-      })
-    })
+  public map: google.maps.Map;
 
-  }
-}
+  constructor(private weatherService: WeatherService) { }
 
+  ngOnInit(): void { }
+
+  //initMap(): void {
+  //  debugger;
+  //  this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+  //    center: { lat: -34.397, lng: 150.644 },
+  //    zoom: 8,
+  //  });
+  //}
 
 }
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,22 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
-url='https://api.openweathermap.org/data/2.5/find?'
-apiKey='de8904fbaac650a197b2565d60b42574'
- 
+  url = 'https://api.openweathermap.org/data/2.5/find?'
+  apiKey = 'de8904fbaac650a197b2565d60b42574'
 
-constructor(private http:HttpClient) { }
-getWeatherDataByChords(lat, lon){
-  let params = new HttpParams()
-  .set('lat', '42.725077')
-  .set('lon', '25.124347')
-  .set('cnt', '50')
-  .set('appid',this.apiKey)
-  return this.http.get(this.url, {params} )
-}
-   
+
+  constructor(private http: HttpClient) { }
+
+  //getWeatherDataByChords(lat, lon){
+  //  let params = new HttpParams()
+  //  .set('lat', '42.725077')
+  //  .set('lon', '25.124347')
+  //  .set('cnt', '50')
+  //  .set('appid',this.apiKey)
+  //  return this.http.get(this.url, {params} )
+  //}
+
 
 }
